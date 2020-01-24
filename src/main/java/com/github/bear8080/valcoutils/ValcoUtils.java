deeff8b6324a9c230bf39c46ba1245f8f4059a1c
Bear8080 <strikinvipers@gmail.com>
2020-01-24 06:28:51 -0600

    First working build.

--- src/main/java/com/github/bear8080/valcoutils/ValcoUtils.java
+++ src/main/java/com/github/bear8080/valcoutils/ValcoUtils.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.github.bear8080.valcoutils;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ValcoUtils.MODID, name = ValcoUtils.NAME, version = ValcoUtils.VERSION)
+public class ValcoUtils
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "valcoutils";
+    public static final String NAME = "Valco Utils";
+    public static final String VERSION = "20.01.24";
 
     private static Logger logger;
 
